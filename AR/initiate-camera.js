464602beb5287f5e11dd39a710077c797c465de3
chrpeterson <christopher.peterson@hpe.com>
2019-03-29 15:25:26 -0500

    Add files via upload

---
+++ AR/initiate-camera.js
@@ -0,0 +1,27 @@
+// Set constraints for the camera
+var constraints = { video: { facingMode: "environment" }, audio: false };
+var track = null;
+
+// Define constants
+const cameraView = document.querySelector("#camera--view"),
+    cameraOutput = document.querySelector("#camera--output"),
+    cameraSensor = document.querySelector("#camera--sensor"),
+    cameraTrigger = document.querySelector("#camera--trigger");
+
+// Access the device camera and stream to cameraView
+function cameraStart() {
+    navigator.mediaDevices
+        .getUserMedia(constraints)
+        .then(function(stream) {
+            track = stream.getTracks()[0];
+            cameraView.srcObject = stream;
+        })
+        .catch(function(error) {
+            console.error("Oops. Something is broken.", error);
+        });
+}
+
+
+
+// Start the video stream when the window loads
+window.addEventListener("load", cameraStart, false);
